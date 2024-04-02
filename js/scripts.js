df5d3a073261a26d1bca012208a1be7a3014f6f3
dhaidous <haidous_21@hotmail.com>
2024-04-02 19:23:13 -0400

    Update scripts.js
    Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-var favoriteFood = "Pizza";
-alert('Hello world');
-document.write("My favorite food is " + favoriteFood + ".");
+pokemonList = [
+    {
+        name: "Bulbasaur",
+        height: 0.7,
+        types: ["grass", "poison"]
+    },
+    {
+        name: "Charmander",
+        height: 0.6,
+        types: ["fire"]
+    },
+    {
+        name: "Squirtle",
+        height: 0.5,
+        types: ["water"]
+    }
+];
