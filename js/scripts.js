e920531bce88c68528712293372963538c77903f
dhaidous <haidous_21@hotmail.com>
2024-04-19 13:20:42 -0400

    loop
    created loop

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,9 @@ pokemonList = [
         types: ["water"]
     }
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemonInfo = pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')';
+    if (pokemonList[i].height > 0.6)
+        pokemonInfo += " - Wow, that’s big!";
+    document.write(pokemonInfo + '<br>');
+}
