2c25cbb657de7c80a64d672f96123de4edca8c78
dhaidous <haidous_21@hotmail.com>
2024-05-08 22:06:23 -0400

    Updated scripts.js
    forEach () Loop and IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,46 @@
-pokemonList = [
-    {
-        name: "Bulbasaur",
-        height: 0.7,
-        types: ["grass", "poison"]
-    },
-    {
-        name: "Charmander",
-        height: 0.6,
-        types: ["fire"]
-    },
-    {
-        name: "Squirtle",
-        height: 0.5,
-        types: ["water"]
+var pokemonRepository = (function () {
+    var pokemonList = [
+        {
+            name: "Bulbasaur",
+            height: 0.7,
+            types: ["grass", "poison"]
+        },
+        {
+            name: "Charmander",
+            height: 0.6,
+            types: ["fire"]
+        },
+        {
+            name: "Squirtle",
+            height: 0.5,
+            types: ["water"]
+        }
+    ];
+
+    function getAll() {
+        return pokemonList;
     }
-];
-for (let i = 0; i < pokemonList.length; i++) {
-    let pokemonInfo = pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')';
-    if (pokemonList[i].height > 0.6)
-        pokemonInfo += " - Wow, that’s big!";
-    document.write(pokemonInfo + '<br>');
-}
+
+    function add(item) {
+        pokemonList.push(item);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
+// Example usage:
+console.log(pokemonRepository.getAll()); // Should return the pokemonList array
+pokemonRepository.add({
+    name: "Pikachu",
+    height: 0.4,
+    types: ["electric"]
+});
+console.log(pokemonRepository.getAll()); // Should return the updated pokemonList array
+
+// forEach loop to iterate over each Pokémon in the repository
+pokemonRepository.getAll().forEach(function (pokemon) {
+    console.log(pokemon.name + ' - Height: ' + pokemon.height + ' - Types: ' + pokemon.types.join(', '));
+});
